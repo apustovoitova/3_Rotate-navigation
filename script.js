ff3218a5bbbd8de884aa5f15ab7cdf0601bea32d
apustovoitova <anastasiapustovoitova@gmail.com>
2024-06-01 10:06:48 +0200

    faq-collapse

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-const jokeEl = document.getElementById("joke");
-const jokeBtn = document.getElementById("jokeBtn");
+const toggles = document.querySelectorAll(".faq-toggle");
 
-jokeBtn.addEventListener("click", generateJoke);
-
-generateJoke();
-
-async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
-
-  const res = await fetch("https://icanhazdadjoke.com", config);
-
-  const data = await res.json();
-
-  jokeEl.innerHTML = data.joke;
-}
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
